5acd49794fff147a5dd9942cab045d7861e734b4
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2024-03-05 18:01:56 +0000

    Add a LambdaRestApi contruct to the stack

--- lib/cdk-workshop-example-stack.ts
+++ lib/cdk-workshop-example-stack.ts
@@ -1,19 +1,18 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
 export class CdkWorkshopExampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkWorkshopExampleQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_16_X, // execution environment
+      code: Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
     });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopExampleTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    });
   }
 }
